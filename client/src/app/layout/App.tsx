b9a4f2ef0d81ed38f1e990f300d3240ed14d6dcd
ThatoR <thatoramphore.info@gmail.com>
2026-03-11 21:44:56 +0200

    UI: update styling

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,15 +1,20 @@
-import { Box, Container, CssBaseline } from "@mui/material";
-import { Outlet } from "react-router";
+import { Box, Container } from "@mui/material";
+import { Outlet, useLocation } from "react-router";
+import HomePage from "../../features/home/HomePage";
 import NavBar from "./NavBar";
 
 function App() {
+  const location = useLocation();
   return (
     <Box sx={{ bgcolor: '#eeeeee', minHeight: '100vh' }}>
-      <CssBaseline />
-      <NavBar />
-      <Container maxWidth="lg" sx={{ mt: 3 }}>
-        <Outlet />
-      </Container>
+      {location.pathname === '/' ? <HomePage /> : (
+        <>
+          <NavBar />
+          <Container maxWidth='xl' sx={{ paddingTop: 8 }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
     </Box>
   )
 }
--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,10 +1,49 @@
-import { Container, Typography } from '@mui/material';
+import { Group } from "@mui/icons-material";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { Link } from "react-router";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const HomePage = () => {
   return (
-    <Container sx={{mt: 3}}>
-        <Typography variant="h3">Home Page</Typography>
-    </Container>
+    <Paper className="home"
+      sx={{
+        color: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 6,
+        alignItems: 'center',
+        alignContent: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'
+      }}
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center', alignContent: 'center', color: 'white', gap: 3 }}>
+        <Group sx={{ height: 110, width: 110 }} />
+        <Typography variant="h1" fontWeight='bold'>
+          Eventra
+        </Typography>
+      </Box>
+      <Typography variant="h2">
+        Welcome to Eventra
+      </Typography>
+      <Button 
+        component={Link} 
+        to='/activities' 
+        size='large' 
+        variant="contained" 
+        endIcon={<ArrowForwardIcon />}
+        sx={{
+          height: 80, 
+          borderRadius: 4, 
+          fontSize: '1.5rem', 
+          backgroundColor: 'white', 
+          color: 'primary.main',
+          px: 3
+        }}>
+        View Activities
+      </Button>
+    </Paper>
   )
 }
 export default HomePage
