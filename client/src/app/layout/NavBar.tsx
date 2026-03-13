86467c208adcdb3c6255a099d1df29d554375d86
ThatoR <thatoramphore.info@gmail.com>
2026-03-13 19:19:26 +0200

    Client-State: manage client state with MobX

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,17 +1,23 @@
 import LocalActivityIcon from '@mui/icons-material/LocalActivity';
-import { Container, MenuItem } from '@mui/material';
+import { Container, LinearProgress, MenuItem } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { NavLink } from 'react-router';
 import MenuItemLink from '../shared/components/MenuItemLink';
+import { useStore } from '../../lib/hooks/useStore';
+import { Observer } from 'mobx-react-lite';
 const NavBar = () => {
+    const { uiStore } = useStore();
+
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={{
-                backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'
-            }}>
+            <AppBar position="static"
+                sx={{
+                    backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)',
+                    position: 'relative'
+                }}>
                 <Container maxWidth='xl'>
                     <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
                         <Box>
@@ -29,6 +35,9 @@ const NavBar = () => {
                             <MenuItemLink to='/createActivity'>
                                 Create Activity
                             </MenuItemLink>
+                            <MenuItemLink to='/counter'>
+                                Counter
+                            </MenuItemLink>
                         </Box>
                         <MenuItem
                         >
@@ -36,6 +45,19 @@ const NavBar = () => {
                         </MenuItem>
                     </Toolbar>
                 </Container>
+                <Observer>
+                    {() => uiStore.isLoading ? (
+                        <LinearProgress color="secondary"
+                            sx={{
+                                position: 'absolute',
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                height: 4
+                            }}
+                        />
+                    ) : null}
+                </Observer>
             </AppBar>
         </Box>
     )
--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,16 +1,19 @@
 import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
 import agent from "../api/agent";
+import { useLocation } from "react-router";
 
 export const useActivities = (id?: string) => {
 
   const queryClient = new QueryClient();
+  const location = useLocation();
 
   const { data: activities, isPending } = useQuery({
     queryKey: ['activities'],
     queryFn: async () => {
       const response = await agent.get<Activity[]>('/activities');
       return response.data;
-    }
+    },
+    enabled: !id && location.pathname === '/activities'
   });
 
   const {data: activity, isLoading: isLoadingActivity} = useQuery<Activity | undefined>({
