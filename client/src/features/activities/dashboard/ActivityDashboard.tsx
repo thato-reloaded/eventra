97cdb2a6d24e25ae6b12a46d5b20e34627232d38
ThatoR <thatoramphore.info@gmail.com>
2026-05-16 11:02:33 +0200

    feat(pagination): set up client pagination

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,13 +1,23 @@
-import { Grid } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import ActivityList from './ActivityList';
 import ActivityFilers from './ActivityFilers';
+import { useActivities } from '../../../lib/hooks/useActivities';
 
 const ActivityDashboard = () => {
+    const {isFetchingNextPage, fetchNextPage, hasNextPage} = useActivities();
 
     return (
         <Grid container spacing={3}>
             <Grid size={8}>
                 <ActivityList />
+                <Button
+                    onClick={() => fetchNextPage()}
+                    sx={{my: 2, float: 'right'}}
+                    variant="contained"
+                    disabled={!hasNextPage || isFetchingNextPage}
+                >
+                    Laod more
+                </Button>
             </Grid>
             <Grid size={4}>
                 <ActivityFilers />
--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,4 +1,4 @@
-import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
+import { QueryClient, useInfiniteQuery, useMutation, useQuery } from "@tanstack/react-query";
 import agent from "../api/agent";
 import { useLocation } from "react-router";
 import { useAccount } from "./useAccount";
@@ -9,24 +9,36 @@ export const useActivities = (id?: string) => {
   const { currentUser } = useAccount();
   const location = useLocation();
 
-  const { data: activities, isLoading } = useQuery({
+  const { data: activitiesGroup, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } 
+    = useInfiniteQuery<PagedList<Activity, string>>({
     queryKey: ['activities'],
-    queryFn: async () => {
-      const response = await agent.get<Activity[]>('/activities');
+    queryFn: async ({ pageParam = null }) => {
+      const response = await agent.get<PagedList<Activity, string>>('/activities', {
+        params: {
+          cursor: pageParam,
+          pageSize: 3
+        }
+      });
       return response.data;
     },
+    initialPageParam: null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
     enabled: !id && location.pathname === '/activities' && !!currentUser,
-    select: data => {
-      return data.map(activity => {
-        const host = activity.attendees.find(x => x.id === activity.hostId);
-        return {
-          ...activity,
-          isHost: currentUser?.id === activity.hostId,
-          isGoing: activity.attendees.some(x => x.id === currentUser?.id),
-          hostImageUrl: host?.imageUrl
-        }
-      })
-    }
+    select: data => ({
+      ...data,
+      pages: data.pages.map((page) => ({
+        ...page,
+        items: page.items.map(activity => {
+          const host = activity.attendees.find(x => x.id === activity.hostId);
+          return {
+            ...activity,
+            isHost: currentUser?.id === activity.hostId,
+            isGoing: activity.attendees.some(x => x.id === currentUser?.id),
+            hostImageUrl: host?.imageUrl
+          }
+        })
+      }))
+    })
   });
 
   const { data: activity, isLoading: isLoadingActivity } = useQuery({
@@ -123,7 +135,10 @@ export const useActivities = (id?: string) => {
   })
 
   return {
-    activities,
+    activitiesGroup,
+    isFetchingNextPage, 
+    fetchNextPage, 
+    hasNextPage,
     isLoading,
     updateActivity,
     createActivity,
