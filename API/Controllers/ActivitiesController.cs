43264e617d5cdbbbe0f0b011de4015ec781fc926
thato-reloaded <thatoramphore.info@gmail.com>
2026-05-16 18:17:38 +0200

    feat(filtering): set up filtering in the API

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,9 +11,10 @@ public class ActivitiesController : BaseApiController
 {
     [AllowAnonymous]
     [HttpGet]
-    public async Task<ActionResult<PagedList<ActivityDto, DateTime?>>> GetActivities(DateTime? cursor)
+    public async Task<ActionResult<PagedList<ActivityDto, DateTime?>>> GetActivities(
+            [FromQuery]ActivityParams activityParams)
     {
-        return HandleResult(await Mediator.Send(new GetActivityList.Query{Cursor = cursor}));
+        return HandleResult(await Mediator.Send(new GetActivityList.Query{Params = activityParams}));
     }
 
     [HttpGet("{id}")]
